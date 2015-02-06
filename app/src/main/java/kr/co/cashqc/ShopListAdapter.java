37d71319f3d4d2753847c3190b842f9f5c3d6950
junghum21 <junghum21@gmail.com>
2015-02-06 10:11:39 +0900

    2015-01-31 ======== 1. 포인트 차등 2. 가맹점 푸시 로그 ======== 2.5.0 65

--- app/src/main/java/kr/co/cashqc/ShopListAdapter.java
+++ app/src/main/java/kr/co/cashqc/ShopListAdapter.java
@@ -250,7 +250,7 @@ public class ShopListAdapter extends BaseAdapter {
                     ribbon = BitmapFactory.decodeResource(mContext.getResources(),
                             R.drawable.img_ribbon_po);
 
-                    h.imgPoint.setImageResource(R.drawable.point_1500);
+                    h.imgPoint.setImageResource(R.drawable.point_2000);
 
                 } else if ("on".equals(pre_pay)) {
 
@@ -286,7 +286,7 @@ public class ShopListAdapter extends BaseAdapter {
 
             } else if (type == TYPE_SILVER) {
 
-                h.separatorText.setText("실버 가맹점 - 1500 포인트 적립");
+                h.separatorText.setText("실버 가맹점 - 2000 포인트 적립");
                 h.separatorImage.setImageResource(R.drawable.list_title_silver);
 
             } else if (type == TYPE_CASHQ) {
